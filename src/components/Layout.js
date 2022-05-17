71a37d890674328a15fbe4bc9dda60f5f95e1c57
Ravi <ravikumr0403@gmail.com>
2022-05-17 22:15:35 +0530

    three col layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,14 +2,16 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { BottomNav } from "./BottomNav";
 import { SideNav } from "./SideNav";
+import { Suggestions } from "./Suggestions";
 
 export const Layout = () => {
   return (
-    <div className="grid-container container mx-auto max-w-screen-lg">
+    <div className="grid-container container mx-auto max-w-screen-xl">
       <SideNav />
-      <main className="min-h-screen w-100 max-w-lg pt-[10vh] pb-[10vh] md:pb-4 md:min-w-[32rem] mx-auto rounded">
+      <main className="min-h-screen w-full px-4 pt-[10vh] pb-[10vh] md:pb-4 rounded">
         <Outlet />
       </main>
+      <Suggestions />
       <BottomNav />
     </div>
   );
---
+++ src/components/Suggestions.js
@@ -0,0 +1,9 @@
+export const Suggestions = () => {
+  return (
+    <aside className="hidden lg:block sticky top-[10vh] h-[85vh] bg-white rounded mt-16 mx-auto p-4 w-full max-w-[18rem] dark:bg-slate-800">
+      <p className="text-semibold text-xl py-2 text-center border-b">
+        People you may know
+      </p>
+    </aside>
+  );
+};
